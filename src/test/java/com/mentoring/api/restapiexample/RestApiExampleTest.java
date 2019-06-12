12ef2ae088a1a206dc30b01e6db9fc1b82a0aa90
Kondriu <dmitriy.vladimirovich@gmail.com>
2019-06-13 00:28:19 +0300

    make some test fail

--- src/test/java/com/mentoring/api/restapiexample/RestApiExampleTest.java
+++ src/test/java/com/mentoring/api/restapiexample/RestApiExampleTest.java
@@ -55,7 +55,7 @@ public class RestApiExampleTest{
 
         Assert.assertEquals(
                 "Account was not deleted",
-                "false",
+                "ZZfalse",
                 restapiexampleService.makeGetRequestById(response.getId()).getBody().asString());
     }
 
