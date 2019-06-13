70c04ac81ce8cf98bdd85de6a71ee50b45e04dc9
Kondriu <dmitriy.vladimirovich@gmail.com>
2019-06-14 01:52:25 +0300

    change smoke

--- src/test/java/com/mentoring/suite/TestSuiteSmokeTest.java
+++ src/test/java/com/mentoring/suite/TestSuiteSmokeTest.java
@@ -1,13 +1,13 @@
 package com.mentoring.suite;
 
-import com.mentoring.api.restapiexample.RestApiExampleTest;
+import com.mentoring.ui.google.search.GoogleSearchTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 
 @Suite.SuiteClasses(
-        RestApiExampleTest.class
+        GoogleSearchTest.class
 )
 
 public class TestSuiteSmokeTest {
